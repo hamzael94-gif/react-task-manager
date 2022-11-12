5afcc3b91056b28e5e8cd5660f4e1ebdc811bed3
hamzael94-gif <elyousfiha68@gmail.com>
2022-11-12 14:14:19 +0100

    update by hamza

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+const Button = ({text,color,onClick}) => {
+  return (
+    <button style={{backgroundColor:color}} className='btn' onClick={onClick}>{text}</button>
+  )
+}
+Button.defaultProps={
+    color:'steelblue',
+}
+// Button.propTypes = {
+//     text: PropTypes.string,
+//     color: PropTypes.string,
+//     onClick: PropTypes.func,
+// }
+
+export default Button
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+  return (
+    <footer>
+    <p>Copyright &copy; 2021</p>
+    <a to='/about'>About</a>
+  </footer>
+  )
+}
+
+export default Footer
